e48a06f30b180d5d595696b146961f91fc78d1d2
2pjava <wiktoriiaolijnyk.33@gmail.com>
2025-09-10 13:21:55 +0200

    java zadania

--- zadania/Z01- powitanie z imieniem/main.js
+++ zadania/Z01- powitanie z imieniem/main.js
@@ -1,8 +1,16 @@
 const imie = document.querySelector("#imie");
 const wynik = document.querySelector("#wynik");
-const btn = document.querySelector("#button");
+const btn = document.querySelector("button");
 
-btn.addEventListener("click", function ()  {})
+btn.addEventListener("click", function ()  {
+
+    let imie_tekst =imie.value;
+    wynik.innerHTML =
+        `
+        <h1> witaj ${imie_tekst} </h1>
+        <h2> miło cię widzieć na naszej sronie.</h2>
+        `
+})
 
 
 
